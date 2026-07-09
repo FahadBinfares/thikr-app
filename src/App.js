79b2a2260fdb74c386bfcab418b07883bd59f631
Fahad <Fahadalfars100@gmail.com>
2026-07-09 15:59:49 +0300

    build main adkar components

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div>
+    <div className="app-container">
       <NavBar />
+      <HeroPart />
+      <AdkarComponentsContiner />
     </div>
   );
 }
@@ -31,6 +33,54 @@ function NavBar() {
           </ul>
         </nav>
       </header>
+    </>
+  );
+}
+
+function HeroPart() {
+  return (
+    <div className="hero-container">
+      <span className="hero-first-tilte"> لا تنسَ وردك</span>
+      <h1 className="main-titel">اطمئنّ قلبُك بذكر الله</h1>
+      <h2 className="expalane-theapp">
+        اختر وردك اليومي من الأذكار الصحيحة، وعُدّها بهدوء بينما يرافقك العدّاد.
+      </h2>
+    </div>
+  );
+}
+
+function AdkarComponentsContiner() {
+  return (
+    <div className=" Adkar-Components-Continer">
+      <AdkarComponents
+        adkarName="أذكار الصباح"
+        adkarTime="من طلوع الفجر حتى العصر"
+        timeLogo="sun-Logo"
+      />
+      <AdkarComponents
+        adkarName="أذكار المساء"
+        adkarTime="من صلاة العصر  حتى الفجر"
+        timeLogo="moon-logo"
+      />
+    </div>
+  );
+}
+
+function AdkarComponents({ adkarName, adkarTime, timeLogo }) {
+  return (
+    <>
+      <div className="adkar-components">
+        <div className="first-line-adkar-components">
+          <div className={timeLogo}></div>
+          <div className="adkar-components-track">
+            <span>اكتمل اليوم</span>
+            <span>13/2</span>
+          </div>
+        </div>
+        <h3 className="adkar-components-title">{adkarName}</h3>
+        <p className="adkar-components-sub-title">{adkarTime}</p>
+        <button className="adkar-components-btn">ابدأ الورد </button>
+      </div>
     </>
   );
 }
