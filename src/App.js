e4d08fa41180a615bd713c5b5c8d8a824501a390
Fahad <Fahadalfars100@gmail.com>
2026-07-13 03:37:16 +0300

    Fix counter While move to another dhikr

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import "./App.css";
-import Landingpage from "./Pages/LandingPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import LandingPage from "./Pages/LandingPage";
+import MorningDhikrPage from "./Pages/MorningDhikrPage";
+import EveningDhikrPage from "./Pages/EveningDhikrPage";
 
 export default function App() {
-  return <Landingpage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/morning" element={<MorningDhikrPage />} />
+        <Route path="/evening" element={<EveningDhikrPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
